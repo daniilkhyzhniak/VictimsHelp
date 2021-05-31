4da9959d16e49192eff5bb2abfb32b4213051e02
daniilkhyzhniak <daniil.khyzhniak@nure.ua>
2021-05-31 11:10:45 +0300

    Add check email availability

--- VictimsHelp/VictimsHelp/VictimsHelp.BLL/Contracts/IUserService.cs
+++ VictimsHelp/VictimsHelp/VictimsHelp.BLL/Contracts/IUserService.cs
@@ -2,7 +2,6 @@
 using System.Security.Claims;
 using System.Threading.Tasks;
 using VictimsHelp.BLL.Models;
-using VictimsHelp.DAL.Entities;
 
 namespace VictimsHelp.BLL.Contracts
 {
---
+++ VictimsHelp/VictimsHelp/VictimsHelp.PL/Controllers/Api/UserApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using VictimsHelp.BLL.Contracts;
+
+namespace VictimsHelp.PL.Controllers.Api
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserApiController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserApiController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("{email}/available")]
+        public async Task<bool> CheckIfEmailIsAvailable(string email)
+        {
+            var user = await _userService.GetByEmailAsync(email);
+
+            return user is null;
+        }
+    }
+}
