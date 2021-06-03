b8e093c83e03a56312de632d71b482eb6124c1d0
daniilkhyzhniak <daniil.khyzhniak@nure.ua>
2021-06-03 22:20:50 +0300

    Add downloading mobile app

--- VictimsHelp/VictimsHelp/VictimsHelp.PL/Controllers/Api/CalendarApiController.cs
+++ VictimsHelp/VictimsHelp/VictimsHelp.PL/Controllers/Api/CalendarApiController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using VictimsHelp.BLL.Contracts;
 using VictimsHelp.BLL.Enums;
--- VictimsHelp/VictimsHelp/VictimsHelp.PL/Controllers/HomeController.cs
+++ VictimsHelp/VictimsHelp/VictimsHelp.PL/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace VictimsHelp.PL.Controllers
 {
     [Route("")]
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class HomeController : Controller
     {
         [HttpGet]
@@ -21,6 +23,15 @@ namespace VictimsHelp.PL.Controllers
         public IActionResult JoinUs()
         {
             return View();
+        }
+
+        [HttpGet("download-mobile-app")]
+        public IActionResult DownloadMobileApp()
+        {
+            var filePath = Path.GetFullPath("victims_help.apk");
+            var fileType = "application/octet-stream";
+
+            return PhysicalFile(filePath, fileType, "victims_help.apk");
         }
     }
 }
